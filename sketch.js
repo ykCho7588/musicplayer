14fe1723b8fa38b708c9eab9c64bfaa43611c2f5
ykCho7588 <89077721+ykCho7588@users.noreply.github.com>
2022-11-25 13:59:47 +0900

    add play/ pause button

--- sketch.js
+++ sketch.js
@@ -1,18 +1,37 @@
-let musicFile;
+//let musicFile;
+
+var song;
+var button;
+
 function setup()
 {
+    createCanvas(200,200);
+    song = loadSound('Gradation.mp3', loadMusic);
+    button = createButton('play');
+    button.mousePressed(togglePlaying);
+    background(51);
     soundFormats('mp3', 'ogg');
-    musicFile = loadSound('Gradation', loadMusic);
+    //musicFile = loadSound('Gradation', loadMusic);
     //musicFile.play();
 
 }
 
+function togglePlaying()
+{
+    if (!song.isPlaying()){
+        song.play();
+        song.setVolume(0.3);
+        button.html('pause');
+    }
+    else
+    {
+        song.stop();
+        button.html('play');
+    }
+}
+
 function loadMusic()
 {
-    musicFile.play();
-}
-function draw()
-{
-    fill(255,0,0);
-    ellipse(50,50,100,100);
+    //musicFile.play();
+    console.log('loaded');
 }
